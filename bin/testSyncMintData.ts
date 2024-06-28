0bbabacbfa4137362b37e1ceb1d6f7ecd55291f8
dingjoys <dingjoysmoon@gmail.com>
2024-06-29 03:24:47 +0800

    mintDataSyncer

--- bin/testSyncMintData.ts
+++ bin/testSyncMintData.ts
@@ -1,16 +1,18 @@
 import { quietSequelize } from "../service/model"
 import { NftTransfer } from "../service/model/nftTransfer"
-
+import moment from 'moment'
 const _main = async () => {
 
     const count = await NftTransfer.count()
+    const now = moment().format()
+
     const sql = `
         insert ignore into nft_mint_data(contract, trace_id, mint_count, max_token_id, total_amount, createdAt, updatedAt)
         VALUEs 
-        (select contract, ${count} as trace_id, count(*) as mint_count, max(token_id) as max_token_id, sum(amount) as total_amount, now(), now() from nft_transfer 
+        (select contract, ${count} as trace_id, count(*) as mint_count, max(token_id) as max_token_id, sum(amount) as total_amount, 
+        ${now} as createdAt, ${now} as updatedAt from nft_transfer 
         where \`from\`=x'0000000000000000000000000000000000000000'
         group by contract)
-
         `
     await quietSequelize.query(sql)
 }
